dadf66775341e4c4c5e7db963e68f8ff1c785a86
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-28 05:09:22 -0700

    df

--- getline.c
+++ getline.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 int main(void)
 {
 	char *inp = NULL;
 	size_t l_inp = 0;
+	int status;
+	pid_t child_pid;
 	
 	while (1)
 	{
@@ -20,19 +24,38 @@ int main(void)
 		}
 		else
 		{
+			/**
 			printf("%s\n", inp);
-			/*printf("the num of the chararcters read is %d\n", rd);
+			printf("the num of the chararcters read is %d\n", rd);
 			printf("i/p was %s\n", inp);
-			*/
+
 			char *tkn = strtok(inp, " \t\n");
 			while (tkn != NULL)
 			{
 				printf("%s\n", tkn);
 				tkn = strtok(NULL, " \t\n");
 			}
+			*/
+			child_pid = fork();
+			if (child_pid == -1)
+			{
+				perror("Error:");
+				return (1);
+			}
+			if (child_pid == 0)
+			{
+				char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+				if (execve(argv[0], argv, NULL) == -1)
+				{
+					perror("Error:");
+				}
+				return (0);
+			}
+			wait(&status);
 		}
-		free(inp);
-		inp = NULL;
+			free(inp);
+			inp = NULL;
 	}
 
 	free(inp);
--- wait_exec.c
+++ wait_exec.c
@@ -12,29 +12,29 @@ int main(void)
 {
 	pid_t child_pid;
 	int status;
+	int i = 0;
 
-	child_pid = fork();
-	if (child_pid == -1)
+	while (i < 5)
 	{
-		perror("Error:");
-		return (1);
-	}
-	if (child_pid == 0)
-	{
-		char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
-
-		printf("Before exec.\n");
-		if (execve(argv[0], argv, NULL) == -1)
+		child_pid = fork();
+		if (child_pid == -1)
 		{
 			perror("Error:");
+			return (1);
 		}
-		printf("After exec.\n");
-		return (0);
-	}
-	else
-	{
+		if (child_pid == 0)
+		{
+			char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			return (0);
+		}
 		wait(&status);
 		printf("Oh, it's all better now\n");
+		i = i + 1;
 	}
 	return (0);
 }
